d6f74c280f836b9259d447512d6454d1844e9119
Joelson <128647252+LucasAlmeidaNogueira@users.noreply.github.com>
2023-05-31 18:45:17 -0300

    Update arquivo2At1.py

--- arquivo2At1.py
+++ arquivo2At1.py
@@ -1,12 +1,7 @@
 #Objetivo: Criar um programa que printe os numeros de uma lista e, depois, eles multiplicados por 2
 #Bonus: Colocar os numeros multiplicados em uma outra lista e printa-la
-import numpy as npy
 
 lista=[2,3,7,12,2]
 
-
-
 for i in lista:
     print (2*i)
-#multiplied = list(npy.array(lista)*2)
-#print(multiplied)
