15056b1bf0aff79343b789d296f16513412a8f2e
Joelson <128647252+LucasAlmeidaNogueira@users.noreply.github.com>
2023-06-28 17:11:45 -0300

    Delete arquivo1At1.py

--- arquivo1At1.py
+++
@@ -1,9 +0,0 @@
-#Objetivo do arquivo: printar qual a posição de um valor da lista
-
-lista=[2,6,1,23,6,8,1]
-x=len(lista)
-
-for i in range(x):
-    
-    
-    print(f"O {x}º valor da lista é a  {lista[x]}")
